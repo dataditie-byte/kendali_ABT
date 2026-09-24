1962f3f46e8c6ca8babb0a539ac8e1001d94587b
dataditie-byte <dataditie@gmail.com>
2026-09-24 07:05:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@
  Untuk pengujian awal frontend, DEMO_MODE=true.
  Untuk produksi, ubah menjadi false.
 */
-const API_URL = '';
-const DEMO_MODE = true;
+const API_URL = 'https://script.google.com/macros/s/AKfycbzCnFZ3rhS1COkGSftXjFuotjmrm1muUhceCPTp_B0A0jKV0Ice4EfX1Nme3sQJruAG/exec';
+const DEMO_MODE = false;
 const APP_VERSION = 'ABT-2026-FINAL-2.0';
